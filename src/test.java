4ee4a70f37f69b5c4c7cdfeb261afce06b31d483
huqi77 <1015758327@qq.com>
2015-09-08 00:37:41 +0800

    项目提交

---
+++ src/test.java
@@ -0,0 +1,15 @@
+
+public class test {
+      int n,m;
+      public void bijiao(int n,int m){
+      if(n<m){
+    	  System.out.print("n小于m");
+      }
+      if(n==m){
+    	 System.out.print("n等于m");
+      }
+      if(n>m){
+    	  System.out.print("n大于m"); 
+      }
+    }
+}
---
+++ src/testTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class testTest {
+	 private static test a = new test();
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBijiao() {
+	a.bijiao(7, 3);
+	}
+
+}
